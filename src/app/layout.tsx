8d23c29380cc2e2c6a9393194b25f5d2ac6d92ec
aleshka <nikolaenkooleksiy@gmail.com>
2025-07-05 17:15:08 +0300

    installed shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Header } from '@/components';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { Header } from '@/components';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
