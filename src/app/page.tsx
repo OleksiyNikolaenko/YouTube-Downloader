f08feb863987e178f83c26139bff4515371b63d6
aleshka <nikolaenkooleksiy@gmail.com>
2025-07-07 17:16:49 +0300

    fix:moved the contents of the page.tsx to a separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { Card, CardContent, CardHeader, SearchBar } from '@/components';
+import { SearchBar } from '@/components';
 
 export default function Home() {
   return (
     <section className="container mx-auto flex h-screen items-center justify-center px-2">
-      <Card className="h-[500px] w-[800px]">
-        <CardHeader className="text-center">
-          <h1 className="text-3xl text-red-700">YouTube Video Downloader</h1>
-        </CardHeader>
-        <CardContent>
-          <SearchBar />
-        </CardContent>
-      </Card>
+      <SearchBar />
     </section>
   );
 }
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,19 @@
+import { Card, CardContent, CardHeader } from '../ui/card';
 import { QualityList } from './QualityList';
 import { SearchInput } from './SearchInput';
 
 export const SearchBar = () => {
   return (
-    <>
-      <SearchInput />
-      <QualityList />
-    </>
+    <Card className="h-[500px] w-[800px]">
+      <CardHeader className="text-center">
+        <h1 className="text-3xl text-red-700">YouTube Video Downloader</h1>
+      </CardHeader>
+      <CardContent>
+        <>
+          <SearchInput />
+          <QualityList />
+        </>
+      </CardContent>
+    </Card>
   );
 };
